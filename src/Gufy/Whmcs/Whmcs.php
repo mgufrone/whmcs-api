9c21f8045ad4475a9010f3a050ce75417e6ccc3a
Mochamad Gufron <mgufronefendi@gmail.com>
2014-07-18 21:38:01 +0700

    add api key feature

--- src/Gufy/Whmcs/Whmcs.php
+++ src/Gufy/Whmcs/Whmcs.php
@@ -8,12 +8,31 @@ class Whmcs
 		// Initiate
 
 		$params['username'] 	= \Config::get('whmcs::username');
-		$params['password'] 	= md5(\Config::get('whmcs::password'));
 		$params['responsetype'] = \Config::get('whmcs::responsetype');
 		$params['action']		= $action;
 
-		// call curl init connection
-		// set url
+		$auth_type = \Config::get('whmcs::auth_type', 'auto');
+
+		switch($auth_type)
+		{
+			case 'auto':
+			if(!empty($params['password']))
+				$params['password'] 	= md5(\Config::get('whmcs::password'));
+			elseif(!empty($params['api_key']))
+				$params['accesskey'] = \Config::get('api_key');
+			break;
+			case 'api':
+			if(false === \Config::has('api_key') || '' === \Config::get('api_key'))
+				throw new \Exception("Please provide api key for authentication");
+			$params['accesskey'] = \Config::get('api_key');
+			break;
+			case 'password':
+			if(false === \Config::has('password') || '' === \Config::get('password'))
+				throw new \Exception("Please provide username password for authentication");
+			$params['password'] 	= md5(\Config::get('whmcs::password'));
+			break;
+		}
+		
 		$url = \Config::get('whmcs::url');
 		// unset url
 		unset($params['url']);
